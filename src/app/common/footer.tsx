6c9fba5a2da2bfbfbbbca4c65d5df799f5a41ebb
lauvAri <198719993@qq.com>
2025-04-03 08:27:46 +0800

    添加资源图片

--- src/app/common/footer.tsx
+++ src/app/common/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <div className="self-end w-full bg-black h-fit text-white
         text-center p-6">
-            <p>2025&copy; All Rights Reserved. <Link href="https://www.csu.edu.cn/">CSU</Link></p>
+            <p>2025&copy; All Rights Reserved by <Link href="https://www.csu.edu.cn/">CSU</Link></p>
         </div>
     )
 }
